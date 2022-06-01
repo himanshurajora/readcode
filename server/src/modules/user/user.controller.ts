640c91200ec8c62642df9c014a7973af1b9c9f0f
himanshurajora <iamahalofan@gmail.com>
2022-06-02 01:37:08 +0530

    username basic controller and services

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('id/:id')
+  async getUserDetailsById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUserDetails(id);
+  }
+
+  @Get('username/:username')
+  async getUserDetailsByUsername(@Param('username') username: string) {
+    return this.userService.getUserByUsername(username);
+  }
+}
--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,6 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/services/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
+
+  async getUserDetails(id: number) {
+    return await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        username: true,
+        projects: {
+          // take only top 5 projects by views
+          select: {
+            title: true,
+          },
+          take: 5,
+          orderBy: {
+            views: 'desc',
+          },
+        },
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  }
+
+  async findIdByUsername(username: string) {
+    return await this.prisma.user.findFirst({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+  }
+
+  async createUser(user: Prisma.UserCreateInput) {
+    return await this.prisma.user.create({
+      data: user,
+    });
+  }
+
+  async getAllUsers() {
+    return await this.prisma.user.findMany();
+  }
+
+  async getUserByUsername(username: string) {
+    const { id } = await this.findIdByUsername(username);
+    return await this.getUserDetails(id);
+  }
 }
